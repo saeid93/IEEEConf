9a552d545f2ee232632f6c525db0a18560ff9155
Saeid Ghafouri <sd_ghafouri@yahoo.com>
2018-11-09 19:09:38 +0330

    first verstion of diffusin matrix-self edge should be added

--- aveGraphs.py
+++ aveGraphs.py
@@ -1,5 +1,13 @@
 import numpy as np
 import os
+import networkx as nx
+
+
+def save_sparse_matrix(m,filename):
+    thefile = open(filename, 'w')
+    nonZeros = np.array(m.nonzero())
+    for entry in range(nonZeros.shape[1]):
+        thefile.write("%s,%s,%s\n" % (nonZeros[0, entry], nonZeros[1, entry], m[nonZeros[0, entry], nonZeros[1, entry]]))
 
 # # number of graph nodes
 numOfNodes = 9877
@@ -12,19 +20,26 @@ adjLists = []
 for file in files:
     adjLists = adjLists + [np.genfromtxt ('simNets/'+file, delimiter=",")]
 
-# build diffusion probability adj list (not memory efficient but fast)
-diffList = np.zeros((numOfNodes,numOfNodes))
-counter = 0
 
+
+# build the graph
+diffGraph = nx.empty_graph(numOfNodes)
 counter = 1
+numOfFiles = 1/100
+diffGraph = nx.empty_graph(numOfNodes)
 for adjList in adjLists:
     numOfele = np.shape(adjList)[0]
     for i in range(0, numOfele):
         rowIndex = int(adjList[i,0]) - 1
         colIndex = int(adjList[i,1]) - 1
-        diffList[rowIndex,colIndex] = diffList[rowIndex,colIndex] + 1
+        if diffGraph.has_edge(rowIndex,colIndex):
+            diffGraph[rowIndex][colIndex]['weight'] = diffGraph[rowIndex][colIndex]['weight'] + numOfFiles
+        else:
+            diffGraph.add_edge(rowIndex,colIndex,weight=numOfFiles)
     print("file "+str(counter)+" completed")
     counter = counter + 1
 
-diffList = diffList/100
-np.savetxt("diffList.csv", diffList, delimiter=",")
+diffGraph = nx.to_scipy_sparse_matrix(diffGraph)
+# print(diffGraph.todense())
+# write matrix to file
+save_sparse_matrix(diffGraph,'diffMatrix.csv')
